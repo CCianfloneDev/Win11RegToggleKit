6a3662351f8b47ba68b9057d5af327cbf48a2bf9
Cole Cianflone <colecianflone123@gmail.com>
2023-12-06 18:30:57 -0600

    Overhauling code
    Realized it was going to be easier to just run .reg files to add/remove the different regedits. Currently Old photo viewer is working, have not made the regedit files for the windows 10 context menu.

--- Win11RegToggleKit/TogglePage.xaml.cs
+++ Win11RegToggleKit/TogglePage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Reflection;
 
 namespace Win11RegToggleKit
 {
@@ -16,123 +17,127 @@ namespace Win11RegToggleKit
         }
 
         /// <summary>
+        /// Runs the registry edit file from the specified location.
+        /// </summary>
+        /// <param name="filePath">Path of .REG file.</param>
+        /// <param name="restartExplorer">Indicates if the regedit requires a explorer.exe restart.</param>
+        private void ApplyRegistryChangesFromFile(string filePath, bool restartExplorer = false)
+        {
+            Process process = new();
+            ProcessStartInfo startInfo = new()
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                FileName = "reg.exe",
+                Arguments = $"import \"{filePath}\"" // Import the .reg file
+            };
+
+            process.StartInfo = startInfo;
+            process.Start();
+            process.WaitForExit();
+
+            if (process.ExitCode == 0)
+            {
+                Debug.WriteLine("Registry changes applied successfully.");
+
+                if (restartExplorer)
+                {
+                    RestartExplorer();
+                }
+            }
+            else
+            {
+                Debug.WriteLine("Failed to apply registry changes.");
+            }
+        }
+
+        private void ApplyRegistryChangesFromResource(string resourceName, bool restartExplorer = false)
+        {
+            using Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+            if (stream != null)
+            {
+                using StreamReader reader = new(stream);
+                string registryContent = reader.ReadToEnd();
+                string temporaryPath = Path.GetTempFileName(); // Create a temporary .reg file
+
+                // Write the content of the embedded resource to the temporary .reg file
+                File.WriteAllText(temporaryPath, registryContent);
+
+                ApplyRegistryChangesFromFile(temporaryPath, restartExplorer);
+
+                // Delete the temporary .reg file after applying changes
+                File.Delete(temporaryPath);
+            }
+            else
+            {
+                Debug.WriteLine("Resource not found.");
+            }
+        }
+
+
+        private void ApplyOldPhotoViewer()
+        {
+            ApplyRegistryChangesFromResource("Win11RegToggleKit.Restore_Windows_Photo_Viewer_CURRENT_USER.reg");
+        }
+
+        private void RemoveOldPhotoViewer()
+        {
+            ApplyRegistryChangesFromResource("Win11RegToggleKit.Undo_Restore_Windows_Photo_Viewer_CURRENT_USER.reg");
+        }
+
+        //private void ApplyWindows10ContextMenu()
+        //{
+        //    string directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location); // Get the directory where the .sln file is located
+        //    string filePath = Path.Combine(directory, "AddWindows10ContextMenu.reg");
+        //    ApplyRegistryChangesFromFile(filePath, restartExplorer: true); // Restart explorer when applying changes
+        //}
+
+        //private void RemoveWindows10ContextMenu()
+        //{
+        //    string directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location); // Get the directory where the .sln file is located
+        //    string filePath = Path.Combine(directory, "RemoveWindows10ContextMenu.reg");
+        //    ApplyRegistryChangesFromFile(filePath);
+        //}
+
+        private void RestartExplorer()
+        {
+            Process[] explorerProcesses = Process.GetProcessesByName("explorer");
+            foreach (Process process in explorerProcesses)
+            {
+                process.Kill();
+            }
+            Process.Start("explorer.exe");
+        }
+
+
+        /// <summary>
         /// Handles the switch toggled event.
         /// </summary>
         private void Switch_Toggled(object sender, ToggledEventArgs e)
         {
             Microsoft.Maui.Controls.Switch switchControl = (Microsoft.Maui.Controls.Switch)sender;
-            Process[] explorerProcesses = Process.GetProcessesByName("explorer");
 
-            List<string> registryKeys = GetRegistryKeys(switchControl.StyleId);
-
-            foreach (string registryKeyPath in registryKeys)
+            if (switchControl.StyleId == switchWin10Menu.StyleId)
             {
-
-                // Check if the registry key exists before attempting to modify it.
-                if (switchControl.IsToggled && RegistryKeyExists(registryKeyPath))
-                {
-                    Debug.WriteLine("Registry key already exists. No modification needed.");
-                    return;
-                }
-
-                Process process = new(); 
-                ProcessStartInfo startInfo = new()
-                {
-                    WindowStyle = ProcessWindowStyle.Hidden,
-                    FileName = "reg.exe"
-                };
-
-                string commandType;
+                //if (switchControl.IsToggled)
+                //{
+                //    ApplyWindows10ContextMenu();
+                //}
+                //else
+                //{
+                //    RemoveWindows10ContextMenu();
+                //}
+            }
+            else if (switchControl.StyleId == switchPhotoViewer.StyleId)
+            {
                 if (switchControl.IsToggled)
                 {
-                    commandType = "created";
-                    startInfo.Arguments = $"ADD HKCU\\{registryKeyPath} /f /ve";
+                    ApplyOldPhotoViewer();
                 }
                 else
                 {
-                    commandType = "deleted";
-                    startInfo.Arguments = $"DELETE HKCU\\{registryKeyPath} /f";
-                }
-
-                process.StartInfo = startInfo;
-                process.Start();
-                process.WaitForExit();
-
-
-                // Check if the registry key creation was successful.
-                if (process.ExitCode == 0)
-                {
-                    Debug.WriteLine($"Registry key {commandType} successfully.");
-                }
-                else
-                {
-                    Debug.WriteLine($"Failed to modify the registry key.");
-                }
-
-                // Restart the windows explorer if we created the reg key, dont need to for deleting. 
-                // only for the windows 10 context menu regedit
-                if (switchControl.StyleId == switchWin10Menu.StyleId && commandType == "created")
-                {
-                    foreach (Process p in explorerProcesses)
-                    {
-                        p.Kill();
-                    }
-
-                    Process.Start("explorer.exe");
+                    RemoveOldPhotoViewer();
                 }
             }
-        }
-
-        /// <summary>
-        /// Indicates if the passed registry key exists or not.
-        /// </summary>
-        /// <param name="keyPath">Path of registry key to check.</param>
-        /// <returns>True if the passed registry key exists on this system or not.</returns>
-        private static bool RegistryKeyExists(string keyPath)
-        {
-            try
-            {
-                using var key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(keyPath);
-                return key != null;
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Error checking registry key: {ex.Message}");
-                return false;
-            }
-        }
-
-        /// <summary>
-        /// Returns the registry key to modify for the given switch name.
-        /// </summary>
-        /// <param name="switchName">Switch associated with registry key.</param>
-        /// <returns>List of Registry Key Path(s).</returns>
-        public static List<string> GetRegistryKeys(string switchName)
-        {
-            List<string> registryKeys = new();
-
-            if (switchName == "switchWin10Menu")
-            {
-                registryKeys.Add("Software\\Classes\\CLSID\\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\\InprocServer32");
-            }
-            if (switchName == "switchPhotoViewer")
-            {
-                registryKeys.Add(@"SOFTWARE\Classes\.bmp");
-                registryKeys.Add(@"SOFTWARE\Classes\.gif");
-                registryKeys.Add(@"SOFTWARE\Classes\.ico");
-                registryKeys.Add(@"SOFTWARE\Classes\.jpeg");
-                registryKeys.Add(@"SOFTWARE\Classes\.jpg");
-                registryKeys.Add(@"SOFTWARE\Classes\.png");
-                registryKeys.Add(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.gif\OpenWithProgids");
-                registryKeys.Add(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.ico\OpenWithProgids");
-                registryKeys.Add(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.jpeg\OpenWithProgids");
-                registryKeys.Add(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.bmp\OpenWithProgids");
-                registryKeys.Add(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.jpg\OpenWithProgids");
-                registryKeys.Add(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.png\OpenWithProgids");
-            }
-             // Add more keys here if needed
-
-            return registryKeys;
         }
     }
 }
